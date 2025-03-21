66df0cb74e9002a6f8b01a57f29d464f21de6efc
Juan Ignacio Adame <jadametum@gmail.com>
2025-03-21 09:13:56 +0900

    Add files via upload

---
+++ postBuild.sh
@@ -0,0 +1,5 @@
+jupyter contrib nbextension install --user
+jupyter nbextension enable --py widgetsnbextension
+jupyter labextension install jupyterlab-plotly --minimize=False
+jupyter labextension install @jupyter-widgets/jupyterlab-manager --minimize=False
+jupyter labextension install plotlywidget --minimize=False
