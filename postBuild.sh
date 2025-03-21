91eb2d7eed2228c9e356848603882dd29db3560a
Juan Ignacio Adame <jadametum@gmail.com>
2025-03-21 09:30:38 +0900

    Delete postBuild.sh

--- postBuild.sh
+++
@@ -1,5 +0,0 @@
-jupyter contrib nbextension install --user
-jupyter nbextension enable --py widgetsnbextension
-jupyter labextension install jupyterlab-plotly --minimize=False
-jupyter labextension install @jupyter-widgets/jupyterlab-manager --minimize=False
-jupyter labextension install plotlywidget --minimize=False
